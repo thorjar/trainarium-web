1adb5d32462761f4c6ec5645cf99ae5957f90284
Jaret <jaretthompson11@gmail.com>
2026-05-21 14:53:39 +0200

    Initial commit

--- next.config.js
+++ next.config.js
@@ -13,9 +13,16 @@ const nextConfig = {
 
 	async rewrites() {
 		return [
+			// Keep NextAuth on Vercel
+			{
+				source: '/api/auth/:path*',
+				destination: '/api/auth/:path*',
+			},
+
+			// Proxy backend API to Railway
 			{
 				source: '/api/:path*',
-				destination: process.env.NEXT_PUBLIC_API_URL + '/:path*',
+				destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
 			},
 		];
 	},
